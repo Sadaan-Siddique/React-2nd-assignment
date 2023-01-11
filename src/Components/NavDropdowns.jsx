75af3a19ecae11b86260ed4ccdb688ad640538e8
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-01-11 21:52:39 +0500

    RelFinish

--- src/Components/NavDropdowns.jsx
+++ src/Components/NavDropdowns.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 function NavDropdowns() {
     return (
         <>
-            <a className="nav-link dropdown-toggle text-light" href="https://github.com/Sadaan-Siddique/React-2nd-assignment" target={'_blank'} id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a className="nav-link dropdown-toggle text-light" rel="noreferrer" href="https://github.com/Sadaan-Siddique/React-2nd-assignment" target={'_blank'} id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Dropdown
             </a>
             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a className="dropdown-item" href="https://github.com/Sadaan-Siddique/React-2nd-assignment" target={'_blank'}>Action</a></li>
-                <li><a className="dropdown-item" href="https://github.com/Sadaan-Siddique/React-2nd-assignment" target={'_blank'}>Another action</a></li>
+                <li><a className="dropdown-item" rel="noreferrer" href="https://github.com/Sadaan-Siddique/React-2nd-assignment" target={'_blank'}>Action</a></li>
+                <li><a className="dropdown-item" rel="noreferrer" href="https://github.com/Sadaan-Siddique/React-2nd-assignment" target={'_blank'}>Another action</a></li>
                 <li><hr className="dropdown-divider" /></li>
-                <li><a className="dropdown-item" href="https://github.com/Sadaan-Siddique/React-2nd-assignment" target={'_blank'}>Something else here</a></li>
+                <li><a className="dropdown-item" rel="noreferrer" href="https://github.com/Sadaan-Siddique/React-2nd-assignment" target={'_blank'}>Something else here</a></li>
             </ul>
         </>
     )
--- src/Components/NavUl.jsx
+++ src/Components/NavUl.jsx
@@ -5,17 +5,17 @@ function NavUl() {
         <>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 h6">
                 <li className="nav-item ">
-                    <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
+                    <a className="nav-link active text-light"  aria-current="page" rel="noreferrer" href="https://www.instagram.com/yasirbro_786/" target={'_blank'}>Home</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link text-light" href="https://www.amazon.com/" target={'_blank'}>Products</a>
+                    <a className="nav-link text-light" rel="noreferrer" href="https://www.amazon.com/" target={'_blank'}>Products</a>
                 </li>
                 <li className="nav-item dropdown">
                     <NavDropdowns/>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link disabled text-light" href="#" tabIndex="-1
-                    " aria-disabled="true">Footer</a>
+                    <a className="nav-link disabled text-light" rel="noreferrer" href="https://www.instagram.com/yasirbro_786/" tabIndex="-1
+                    " aria-disabled="true" target={'_blank'}>Footer</a>
                 </li>
             </ul>
         </>
